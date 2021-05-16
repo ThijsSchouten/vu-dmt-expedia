9d6d493dfc5ba920c0f9a12fd430cc27945a1b30
Siem Teusink <siemteusink@hotmail.com>
2021-05-16 18:05:46 +0200

    Added price_diff feature

--- preprocess_data.py
+++ preprocess_data.py
@@ -8,7 +8,7 @@ def main():
     random.seed(42)
     np.random.seed(42)
     data = read_datafile("./data/training_set_VU_DM.csv", all_data=False)
-    data = normalise_price(data)
+    data = normalise_column(data, "price_usd")
     data = drop_and_impute(data)
     data = balance_click_classes(data)
     with open("./data/normalised_data.pickle", "wb") as f:

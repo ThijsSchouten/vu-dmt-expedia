61601e533477dc3de098adcdfb02a0dc1b98c55e
TE Schouten <thijs.schouten@gmail.com>
2021-05-17 18:19:49 +0200

    generate file for feature importance analysis

--- script_LTR.py
+++ script_LTR.py
@@ -1,48 +1,69 @@
+# %%
 from lib_LTR import *
+import pickle
 
+ID = "A1"
+
+# %%
 # Initialize ranker
 ranker = LearnToRank()
 
 ranker.load_data(
-    # "./data/normalised_unbalanced_training-data.pickle",
-    "./data/normalised_data.pickle",
-    "./data/normalised_test-data.pickle",
+    train_df_pickle="./data/split/train80prc.pickle",  # "./data/normalised_data.pickle"
+    test_df_pickle="./data/normalised_test-data.pickle",
+    val_df_pickle="./data/split/val20prc.pickle",
 )
 
-ranker.add_rank()
+ranker.add_rank(click_score=1, book_score=5)
 ranker.add_qid(att="srch_id")  # query ID / group
-
-# Then, prepare the data. Make
-# sure train&test columns match
 ranker.prep_data(drop_cols=True)
+# ranker.create_groupsplits(test_size=0.25, splits=2)
 
-# %% Split the data by groups and run gridsearch
-ranker.create_groupsplits(test_size=0.25, splits=2)
-
-# Run gridsearch
-fit_params = dict(
-    tree_method="hist", booster="gbtree", objective="rank:ndcg", random_state=42,
-)
-
-grid_params = dict(
-    learning_rate=[0.1, 0.05, 0.005],
+# %% Run gridsearch
+params = dict(
+    tree_method=["hist"],
+    booster=["gbtree"],
+    objective=["rank:ndcg"],
+    # random_state=[42],
     colsample_bytree=[1, 0.9, 0.5],
-    eta=[0.05, 0.005],
-    max_depth=[9, 12, 15],
-    n_estimators=[90, 60, 30],
+    eta=[0.1, 0.05, 0.005],
+    max_depth=[5, 7, 9, 12, 15],
+    n_estimators=[60, 35, 20],
     subsample=[0.9, 0.6, 0.4],
 )
-# grid_params = dict(max_depth=[7], n_estimators=[7])
-
-ranker.gridsearch(
-    fit_params, grid_params, out_path="output/gridsearch_02_unbalanced.pickle"
+params = dict(
+    compute_importances=[True],
+    tree_method=["hist"],
+    booster=["gbtree"],
+    objective=["rank:pairwise"],
+    # random_state=[42],
+    colsample_bytree=[0.9],
+    eta=[0.1],
+    max_depth=[9],
+    n_estimators=[150],
+    subsample=[0.9],
 )
-grouped = ranker.get_best_params()
+
+ranker.gridsearch(params, out_path=f"output/results/{ID}_gridsearch.pickle")
+gridsearch_results = ranker.gridsearch_results
 
 # %%
-ranker.load_results("output/gridsearch_02_unbalanced.pickle")
+# ranker.load_results("output/gridsearch_0X_TEST.pickle")
 ranker.get_best_params()
-ranker.train_best_model("output/resultsY.csv")
+ranker.train_best_model(f"output/results/{ID}_kaggle_pred.csv")
+ranker.best_model.save_model(f"output/results/{ID}_best_model.model")
 
-# %% ToDo: analyze feature importances
-res = ranker.results_aggregated
+#%% Write files to pickle for later analysis
+fi = ranker.best_model.feature_importances_
+names = ranker.X_trn.columns
+bm = ranker.best_model
+X_val = ranker.X_val
+y_val = ranker.y_val
+out_obj = dict(fi=fi, names=names, model=bm, X_val=X_val, y_val=y_val)
+
+fname = f"output/results/{ID}_feature_importances.p"
+pickle.dump(out_obj, open(fname, "wb"))
+
+# feature_ranker.best_model.feature_importances_
+
+# %%

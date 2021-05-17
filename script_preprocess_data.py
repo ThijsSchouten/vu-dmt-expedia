2633e80843c79e2ed50d0077fb7717bbb3e69568
Siem Teusink <siemteusink@hotmail.com>
2021-05-17 09:09:03 +0200

    Minor changes, added difference price and price recent feature to preprocessing script.

--- script_preprocess_data.py
+++ script_preprocess_data.py
@@ -8,11 +8,18 @@ def main():
     random.seed(42)
     np.random.seed(42)
     data = read_datafile("./data/training_set_VU_DM.csv", all_data=False)
+
+    # Normalisation
     data = normalise_column(data, "price_usd")
     data = normalise_column(data, "prop_location_score1")
     data = normalise_column(data, "prop_location_score2")
+
+    # Missing values and class balancing
     data = drop_and_impute(data)
     data = balance_click_classes(data)
+
+    # Add features
+    add_pricediff_feature(data, inplace=True)
 
     # Define features to create combinatorial collumns from
     comb_feats = [
